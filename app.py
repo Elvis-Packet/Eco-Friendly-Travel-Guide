62a7c5bd44b34224cecfbb57b58c8ac0776844c4
eddy <eddy.mwaniki.27@gmail.com>
2025-04-09 10:38:03 +0300

    Adds gunicorn

--- app.py
+++ app.py
@@ -3,10 +3,13 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS 
+
 
 from models import db, Destination, Activity, Review, DestinationActivity, TravelTip
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecofriendlydestinations.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
